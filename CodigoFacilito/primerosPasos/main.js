8b7e73aee0132928aad23e325823e879d5d0ecde
IgnaDev <arcortoon@gmail.com>
2023-10-10 18:49:50 -0300

    Configurado tailwind

--- CodigoFacilito/primerosPasos/main.js
+++ CodigoFacilito/primerosPasos/main.js
@@ -6,6 +6,6 @@ const texto = document.createElement('h1');
 texto.innerText = 'Hola Mundo';
 //Agrega las clases text-3xl font-bold underline
 
-texto.classList.add('text-3xl', 'underline');
+texto.classList.add('text-3xl', 'font-bold', 'underline');
 
 app.appendChild(texto);
