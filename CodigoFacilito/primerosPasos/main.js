fed8411472b22ff275f30eddb711e8e36d96abd8
IgnaDev <arcortoon@gmail.com>
2023-10-11 11:14:47 -0300

    Tamaño de texto

--- CodigoFacilito/primerosPasos/main.js
+++ CodigoFacilito/primerosPasos/main.js
@@ -5,6 +5,6 @@ const app = document.getElementById('app');
 
 app.innerHTML = `
   <div class="container lg:bg-red-400  sm:bg-green-400">
-    <h1>Hello World</h1>
+    <h1 class="text-6xl md:text-4xl" > Hello World </h1>
   </div>
 `;
