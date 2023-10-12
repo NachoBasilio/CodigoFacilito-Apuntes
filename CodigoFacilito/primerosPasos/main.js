6b86bd96800beb283b620480ce02daff74f5735a
IgnaDev <arcortoon@gmail.com>
2023-10-12 10:01:57 -0300

    decordaores

--- CodigoFacilito/primerosPasos/main.js
+++ CodigoFacilito/primerosPasos/main.js
@@ -8,7 +8,7 @@ app.innerHTML = `
     <h1 class="text-6xl md:text-4xl" > Hello World </h1>
     <h2 class="bg-indigo-400 mt-4 ml-3 mr-2 mb-3" > Hola a todos</h2>
     <h2 class="bg-indigo-400 xl:my-2" > Hola a todos</h2>
-    <button class="bg-indigo-400 px-4 py-2 hover:bg-indigo-500 hover:text-indigo-50" >Hover me</button>
+    <button class="bg-indigo-400 ring-4  ring-offset-4 hover:rounded px-4 py-2 hover:bg-indigo-500 hover:text-indigo-50" >Hover me</button>
     <input type="text" class="focus:bg-blue-400 focus:text-blue-50 pl-2">
     <p class="text-left text-red-300">Buenas</p>
     <p class="text-center font-bold">Buenas</p>
